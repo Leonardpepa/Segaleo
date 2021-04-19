fa0db8ed68f91ca0b703fa90f344550930e8f043
Aravella Lousta <aravellalousta01@gmail.com>
2021-04-19 23:19:33 +0300

    Created classes FAQ, FAQReader and txt file Q&A

---
+++ src/contact/FAQReader.java
@@ -0,0 +1,67 @@
+package contact;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/* This class is used to read the data from the file 
+ * that contains the frequent questions and answers
+ * about the services*/
+public class FAQReader {
+	
+	private ArrayList<FAQ> faqsList;
+	
+	//Initializing the list
+	public FAQReader() {
+		faqsList = new ArrayList<>();
+		readFAQ();
+	}
+	
+	private void readFAQ() {
+		
+		String line;
+		String question;
+		String answer;
+		
+		//File format: the # is used to separate the question from the answer
+		File qnaFile = new File("files\\faqs\\Q&A");
+		try {
+			FileReader reader = new FileReader(qnaFile);
+			BufferedReader inputReader = new BufferedReader(reader);
+			
+			/* The reader reads each line, 
+			 * separates the question from the answer,
+			 * adds it to the list */
+			line = inputReader.readLine();
+			while(line != null) {
+				// replacing the \n from the file with a line separator
+				question = line.split("#")[0].replace("\\n", System.lineSeparator());
+				answer = line.split("#")[1].replace("\\n", System.lineSeparator());
+				faqsList.add(new FAQ(question, answer));
+				line = inputReader.readLine();
+			}
+			inputReader.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public ArrayList<FAQ> getFaqsList() {
+		return faqsList;
+	}
+
+	public void setFaqsList(ArrayList<FAQ> faqsList) {
+		this.faqsList = faqsList;
+	}
+	
+	
+}
