68a163b1f22771da28987e2d82fe2972bc52090c
Leonard Pepa <leopepp4@gmail.com>
2021-04-24 21:31:59 +0300

    refactoring the scrollablePanel to hold vertical && horizontal

--- src/gui/factory/MenuHeader.java
+++ src/gui/factory/MenuHeader.java
@@ -1,6 +1,5 @@
 package gui.factory;
 
-
 import java.awt.*;
 import java.awt.Dimension;
 import java.util.ArrayList;
@@ -21,36 +20,36 @@ public class MenuHeader extends JPanel {
 	private JPanel container;
 	private JLabel dealsOfTheDay;
 	private ArrayList<Product> deals;
-	
+
 	public MenuHeader(ArrayList<Product> deals) {
 		this.deals = deals;
 		this.setPreferredSize(new Dimension(375, 260));
 		this.setLayout(null);
-		
+
 		configureComponents();
-		
+
 		container.setLayout(new BorderLayout());
-		container.add(new HorizontalScrollablePanel().fill(deals));
-		
+		container.add(new ScrollablePanel().createHorizontal(deals));
+
 		this.add(backButton);
 		this.add(search);
 		this.add(dealsOfTheDay);
 		this.add(container);
 	}
-	
+
 	public void configureComponents() {
 		backButton = ButtonFactory.createButtonIcon(new ImageIcon("./buttonImages/Back Button.png"));
 		backButton.setBounds(16, 30, 63, 23);
-		
-		search = TextFieldFactory.createTextField(TextResources.search, Color.white, Color.GRAY, FontFactory.poppins(14));
+
+		search = TextFieldFactory.createTextField(TextResources.search, Color.white, Color.GRAY,
+				FontFactory.poppins(14));
 		search.setBounds(16, 70, 340, 30);
-		
+
 		container = new JPanel();
 		container.setBounds(16, 140, 340, 200);
-		
+
 		dealsOfTheDay = LabelFactory.createLabel(TextResources.deals, Color.BLACK, FontFactory.poppins(16));
-		dealsOfTheDay.setBounds(16, 110,200, 30);
+		dealsOfTheDay.setBounds(16, 110, 200, 30);
 	}
-	
-	
+
 }
