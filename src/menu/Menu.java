e5b9cb1473e2544ba12ec8d8844c0add6ef14f44
Leonard Pepa <leopepp4@gmail.com>
2021-04-26 17:13:43 +0300

    reviewed and refactored the menu class

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,6 +1,5 @@
 package menu;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,95 +9,77 @@ import java.util.ArrayList;
 
 import order.Food;
 import order.Product;
+import resources.TextResources;
 
 public class Menu {
-	
-	private ArrayList<Product> appetizersListENG;
-	private ArrayList<Product> appetizersListGR;
-	private ArrayList<Product> mainListENG;
-	private ArrayList<Product> mainListGR;
-	private ArrayList<Product> saladsListENG;
-	private ArrayList<Product> saladsListGR;
-	
+
+	private ArrayList<Product> appetizersList;
+	private ArrayList<Product> mainList;
+	private ArrayList<Product> saladsList;
+
 	private String line;
 	private String nameOfFood;
 	private String description;
 	private double price;
 	private String path;
-	
+
 	public Menu() {
-		appetizersListENG = new ArrayList<>();
-		appetizersListGR = new ArrayList<>();
-		mainListENG = new ArrayList<>();
-		mainListGR = new ArrayList<>();
-		saladsListENG = new ArrayList<>();
-		saladsListGR = new ArrayList<>();
-		
-		readingMethod(appetizersListENG, "files\\appetizers\\AppetizersEnglish");
-		readingMethod(appetizersListGR, "files\\appetizers\\AppetizersGreek");
-		readingMethod(mainListENG, "files\\main\\MainEnglish");
-		readingMethod(mainListGR, "files\\main\\MainGreek");
-		readingMethod(saladsListENG, "files\\salads\\SaladsEnglish");
-		readingMethod(saladsListGR, "files\\salads\\SaladsGreek");
+		appetizersList = new ArrayList<>();
+		mainList = new ArrayList<>();
+		saladsList = new ArrayList<>();
+
+		readFood(appetizersList, "files\\appetizers\\Appetizers", TextResources.endpointPath);
+		readFood(mainList, "files\\main\\Main", TextResources.endpointPath);
+		readFood(saladsList, "files\\salads\\Salads", TextResources.endpointPath);
 	}
-	
-	private void readingMethod(ArrayList<Product> list, String pathName) {
-		
-		File activitieFile = new File(pathName);
+
+	private void readFood(ArrayList<Product> list, String pathName, String language) {
+
+		File activitieFile = new File(pathName + language);
 		try {
 			FileReader reader = new FileReader(activitieFile);
-			BufferedReader inputReader = new BufferedReader (reader);
+			BufferedReader inputReader = new BufferedReader(reader);
 
-			/*The reader reads each line,
-			 * separates name, price, description and
-			 * adds it into the list
+			/*
+			 * The reader reads each line, separates name, price, description and adds it
+			 * into the list
 			 */
 			line = inputReader.readLine();
-			
+
 			while (line != null) {
-				//separation
+				// separation
 				nameOfFood = line.split("#")[0].replace("\\n", System.lineSeparator());
 				description = line.split("#")[1].replace("\\n", System.lineSeparator());
-				//convertion into Double
-				price = Double.parseDouble(line.split("#")[2].replace("\\n", System.lineSeparator()));
-				path = line.split("#")[3].replace("\\n", System.lineSeparator());
+				// convertion into Double
+				price = Double.parseDouble(line.split("#")[2]);
+				path = line.split("#")[3];
+
+				list.add(new Food(nameOfFood, description, price, path));
 				
-				list.add(new Food(nameOfFood, description, price, path));				
-		}
-		inputReader.close();
-		reader.close();
-		}
-		catch(FileNotFoundException e) {
+				line = inputReader.readLine();
+
+			}
+			inputReader.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	
-	public ArrayList<Product> getAppetizersListENG(){
-		return appetizersListENG;
+	public ArrayList<Product> getAppetizersList() {
+		return appetizersList;
 	}
-	
-	public ArrayList<Product> getAppetizersListGR(){
-		return appetizersListGR;
+
+
+	public ArrayList<Product> getMainList() {
+		return mainList;
 	}
-	
-	public ArrayList<Product> getMainListENG(){
-		return mainListENG;
+
+	public ArrayList<Product> getSaladsList() {
+		return saladsList;
 	}
-	
-	public ArrayList<Product> getMainListGR(){
-		return mainListGR;
-	}
-	
-	public ArrayList<Product> getSaladsListENG(){
-		return saladsListENG;
-	}
-	
-	public ArrayList<Product> getSaladsListGR(){
-		return saladsListGR;
-	}
+
 }
