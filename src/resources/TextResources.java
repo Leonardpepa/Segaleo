d75a8218994429ed8aace2238b236df97b6bb0fa
Leonard Pepa <leopepp4@gmail.com>
2021-04-27 17:10:48 +0300

    changed the greek text in the text resources

--- src/resources/TextResources.java
+++ src/resources/TextResources.java
@@ -27,16 +27,16 @@ public class TextResources {
 	public static String endpointPath;
 
 	public  void changeLanguage() {
-		greek = isEnglish ? "Greek" : "Î•Î»Î»Î·Î½Î¹ÎºÎ¬";
-		english = isEnglish ? "English" : "Î‘Î³Î³Î»Î¹ÎºÎ¬";
-		roomService = isEnglish ? "ROOM SERVICE" : "Î Î‘Î¡Î‘Î“Î“Î•Î›Î™Î‘";
-		activities = isEnglish ? "ACTIVITIES" : "Î”Î¡Î‘Î£Î¤Î—Î¡Î™ÎŸÎ¤Î—Î¤Î•Î£";
-		welcome = isEnglish ? "Welcome to Segaleo" : "ÎšÎ±Î»ÏŽÏ‚ Î®Ï�Î¸Î±Ï„Îµ ÏƒÏ„Î¿ Segaleo";
-		loginto = isEnglish ?  "Login to your account" : "Î£Ï…Î½Î´ÎµÎ¸ÎµÎ¯Ï„Îµ ÏƒÏ„Î¿Î½ Î»Î¿Î³Î±Ï�Î¹Î±ÏƒÎ¼ÏŒ ÏƒÎ±Ï‚";
-		roomField = isEnglish ?  "  Room Number" : "  Î‘Ï�Î¹Î¸Î¼ÏŒÏ‚ Î´Ï‰Î¼Î±Ï„Î¯Î¿Ï…";
-		passField = isEnglish ? "  Password" : "  ÎšÏ‰Î´Î¹ÎºÏŒÏ‚";
-		loginBtn = isEnglish ? "LOGIN" : "Î•Î™Î£ÎŸÎ”ÎŸÎ£";
-		languageLabel = isEnglish ? "Languages" : "Î“Î»ÏŽÏƒÏƒÎµÏ‚";
+		greek = isEnglish ? "Greek" : "Ελληνικά";
+		english = isEnglish ? "English" : "Αγγλικά";
+		roomService = isEnglish ? "ROOM SERVICE" : "ΠΑΡΑΓΓΕΛΙΑ";
+		activities = isEnglish ? "ACTIVITIES" : "ΔΡΑΣΤΗΡΙΟΤΗΤΕΣ";
+		welcome = isEnglish ? "Welcome to Segaleo" : "Καλώς ήρθατε στο Segaleo";
+		loginto = isEnglish ?  "Login to your account" : "Συνδεθείτε στον λογαριασμό σας";
+		roomField = isEnglish ?  "  Room Number" : "  Αριθμός Δωματίου";
+		passField = isEnglish ? "  Password" : "  Κωδικός‚";
+		loginBtn = isEnglish ? "LOGIN" : "Σύνδεση";
+		languageLabel = isEnglish ? "Languages" : "Γλώσσες";
 		viewCart = isEnglish ? "View Cart" : "Δες το καλαθι";
 		search = isEnglish ? "Search" : "Αναζητηση";
 		deals = isEnglish ? "Deals of the day" : "Προσφορες της ημερας";
