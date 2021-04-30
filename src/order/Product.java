2ba80225092a7ec963acda8f036842e0ab39b0b5
Leonard Pepa <leopepp4@gmail.com>
2021-04-30 21:48:21 +0300

    added array list property to the Product class && method to add rating

--- src/order/Product.java
+++ src/order/Product.java
@@ -1,18 +1,29 @@
 package order;
 
+import java.util.ArrayList;
+
+import rating.Rating;
+
 public abstract class Product {
 	private String name;
 	private String description;
 	private double price;
 	private String path;
+	private ArrayList<Rating> ratings;
 	
 	public Product(String name, String description, double price, String path) {
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.path = path;
+		this.ratings = new ArrayList<Rating>();
 	}
 
+	
+	public void addRating(Rating rating) {
+		ratings.add(rating);
+	}
+	
 	public String getName() {
 		return name;
 	}
