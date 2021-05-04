17ddf199753680ea6215d9f39226606742acb024
Evita <dasyevita@gmail.com>
2021-05-04 22:42:27 +0300

    add methods in PaymentsMethods class

--- src/gui/windows/PaymentMethodsWindow.java
+++ src/gui/windows/PaymentMethodsWindow.java
@@ -1,5 +1,7 @@
 package gui.windows;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,6 +10,11 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import gui.factory.ButtonFactory;
+import gui.factory.FontFactory;
+import gui.factory.LabelFactory;
+import resources.TextResources;
 
 public class PaymentMethodsWindow extends JFrame  implements ActionListener {
 	private static final long serialVersionUID = 1L;
@@ -38,10 +45,120 @@ public class PaymentMethodsWindow extends JFrame  implements ActionListener {
 //		showWindow(this,true);
 	}
 
+	//settings for the frame
+	public void windowsConfiguration() {
+		this.setTitle("Hotel PDA Sample");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+	}
+
+	//set up the panel in the frame
+	public void initializePanelToFrame() {
+		
+		new TextResources().changeLanguage();
+		panel = new JPanel();
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(375, 812));
+
+
+		configureLabels();
+		configureButtons();
+		configureBackground();
+		addListeners();
+		addComponentsToPanel();
+		
+		panel.setBackground(Color.white);
+		this.setContentPane(panel);
+		this.pack();
+	}
+
+	//makes the frame visible
+	public void showWindow(JFrame frame, boolean show) {
+		frame.setVisible(show);
+	}
+
+	//label settings
+	public void configureLabels() {
+		paymentLabel = LabelFactory.createLabel(TextResources.payment, Color.black, FontFactory.poppins(22));
+		paymentLabel.setBounds(24, 73, 200, 31);
+	
+	}
+
+	//buttons settings
+	public void configureButtons() {
+		
+		
+		if (TextResources.isEnglish)
+		{
+			cardBtn = ButtonFactory.createButtonIcon(cardImage);
+			cardBtn.setPressedIcon(cardImage);
+		}
+		else
+		{
+			cardBtn = ButtonFactory.createButtonIcon(cardImageGR); //na valw ta ellhnika 
+			cardBtn.setPressedIcon(cardImageGR);
+		}
+		cardBtn.setBounds(17, 154, 342, 133);
+		
+		paypalBtn = ButtonFactory.createButtonIcon(paypalImage);
+		paypalBtn.setBounds(17, 309 , 342, 133);
+		paypalBtn.setPressedIcon(paypalImage);
+		
+		if (TextResources.isEnglish)
+		{
+			roomBillBtn = ButtonFactory.createButtonIcon(roomBillImage);
+			roomBillBtn.setPressedIcon(roomBillImage);
+		}
+		else
+		{
+			roomBillBtn = ButtonFactory.createButtonIcon(roomBillImageGR);//na valw ta ellhnika
+			roomBillBtn.setPressedIcon(roomBillImageGR);
+		}
+		roomBillBtn.setBounds(17, 467 , 342, 133);
+
+	}
+	
+	public void configureBackground() {
+		backgroundLabel = new JLabel(backgroundImage);
+		backgroundLabel.setBounds(0,0,375, 125);
+	}
+
+	public void addListeners() {
+		cardBtn.addActionListener(this);
+		paypalBtn.addActionListener(this);
+		roomBillBtn.addActionListener(this);
+
+	}
+
+	public void addComponentsToPanel() {
+		panel.add(paymentLabel);
+		panel.add(cardBtn);
+		panel.add(paypalBtn);
+		panel.add(roomBillBtn);
+		
+		panel.add(backgroundLabel);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+
+		if(e.getSource() == cardBtn) {
+//			this.dispose();
+//			new CardWindow();
+		}
 		
+		if(e.getSource() == paypalBtn) {
+//			this.dispose();
+//			new PaypalWindow();
+			
+		}
+		if(e.getSource() == roomBillBtn) {
+//			this.dispose();
+//			new RoomBillWindow();
+			
+		}
+		initializePanelToFrame();
 	}
+
 }
