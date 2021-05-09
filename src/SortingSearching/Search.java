7e1860877a74d43b8a9aa5628e34ff42888f460a
Evita <dasyevita@gmail.com>
2021-05-09 14:22:40 +0300

    no message

--- src/SortingSearching/Search.java
+++ src/SortingSearching/Search.java
@@ -8,53 +8,53 @@ import order.Product;
 import java.util.Comparator;
 
 public class Search {
-    
-    private ArrayList<Product> array= new ArrayList<Product>();
-   
-    public ArrayList<Product> ExpoSearch(ArrayList<Product> prod_array, String KeyWord)
-    {
-        
-        this.array = prod_array;
-        new Sort();
-        Collections.sort(array, Sort.prodNameComparator);
-       
-        int i=1;
-        String name=array.get(i).getName().substring(0, KeyWord.length());
 
-        while(i<array.size() && name.compareTo(KeyWord)<=0)
-        {
-            i=i*2;
-            name = array.get(i).getName().substring(0, KeyWord.length());
-            if(i*2>=array.size())
-            {
-                return SerSearch(array.size(), i-1, KeyWord, array);
-                
-            }
-        }
-        if(i<array.size())
-        {
-            int start=i/2;
-            return SerSearch(i,start,KeyWord, array);
-        }
-        else {return null;}
-    } 
+	private ArrayList<Product> array = new ArrayList<Product>();
 
-    public ArrayList<Product> SerSearch(int end, int start, String KeyWord, ArrayList<Product> array)
-    {
-        this.array = array;
-        ArrayList<Product> Found = new ArrayList<Product>();
+	public ArrayList<Product> expoSearch(ArrayList<Product> prod_array, String KeyWord)
+	{
 
-        for(int i=start;i<end;i++)
-        {
-            String name=array.get(i).getName().substring(0, KeyWord.length());
-            if(name.equals(KeyWord))
-            {
-                Found.add(array.get(i));
-            }
-        }
-        return Found;
-    }
+		this.array = prod_array;
+		new Sort();
+		Collections.sort(array, Sort.prodNameComparator);
 
-    
+		int i=1;
+		String name=array.get(i).getName().substring(0, KeyWord.length());
+
+		while(i<array.size() && name.compareTo(KeyWord)<=0)
+		{
+			i=i*2;
+			name = array.get(i).getName().substring(0, KeyWord.length());
+			if(i*2>=array.size())
+			{
+				return serSearch(array.size(), i-1, KeyWord, array);
+
+			}
+		}
+		if(i<array.size())
+		{
+			int start=i/2;
+			return serSearch(i,start,KeyWord, array);
+		}
+		else {return null;}
+	} 
+
+	public ArrayList<Product> serSearch(int end, int start, String KeyWord, ArrayList<Product> array)
+	{
+		this.array = array;
+		ArrayList<Product> Found = new ArrayList<Product>();
+
+		for(int i=start;i<end;i++)
+		{
+			String name=array.get(i).getName().substring(0, KeyWord.length());
+			if(name.equals(KeyWord))
+			{
+				Found.add(array.get(i));
+			}
+		}
+		return Found;
+	}
+
+
 
 }
--- src/SortingSearching/Sort.java
+++ src/SortingSearching/Sort.java
@@ -5,66 +5,66 @@ import java.util.Comparator;
 import order.Product;
 
 public class Sort {
-    
-    public static Comparator<Product> prodNameComparator= new Comparator<Product>() {
 
-        public int compare(Product prod1, Product prod2) {
-            String ProductName1 = prod1.getName();
-            String ProductName2 = prod2.getName();
+	public static Comparator<Product> prodNameComparator = new Comparator<Product>() {
 
-            
-            return ProductName1.compareTo(ProductName2);
-
-        }
-    };
+		public int compare(Product prod1, Product prod2) {
+			String ProductName1 = prod1.getName();
+			String ProductName2 = prod2.getName();
 
 
-    public static Comparator<Product> AscProdPriceComparator = new Comparator<Product>() {
+			return ProductName1.compareTo(ProductName2);
 
-        public int compare(Product prod1, Product prod2) {
-            double ProductPrice1 = prod1.getPrice();
-            double ProductPrice2 = prod2.getPrice();
+		}
+	};
 
-            if (ProductPrice1 < ProductPrice2)
-                return 1;
-            if (ProductPrice1 > ProductPrice2)
-                return -1;
-            return 0;
 
-        }
-    };
+	public static Comparator<Product> AscProdPriceComparator = new Comparator<Product>() {
 
-    public static Comparator<Product> DesProdPriceComparator = new Comparator<Product>() {
+		public int compare(Product prod1, Product prod2) {
+			double ProductPrice1 = prod1.getPrice();
+			double ProductPrice2 = prod2.getPrice();
 
-        public int compare(Product prod1, Product prod2) {
-            double ProductPrice1 = prod1.getPrice();
-            double ProductPrice2 = prod2.getPrice();
+			if (ProductPrice1 < ProductPrice2)
+				return 1;
+			if (ProductPrice1 > ProductPrice2)
+				return -1;
+			return 0;
 
-            if ( ProductPrice1 < ProductPrice2)
-                return -1;
-            if (ProductPrice1 > ProductPrice2)
-                return 1;
-            return 0;
+		}
+	};
 
-        }
+	public static Comparator<Product> DesProdPriceComparator = new Comparator<Product>() {
 
-        
-    };
+		public int compare(Product prod1, Product prod2) {
+			double ProductPrice1 = prod1.getPrice();
+			double ProductPrice2 = prod2.getPrice();
 
-    public static Comparator<Product> ProdRatingComparator = new Comparator<Product>() {
+			if ( ProductPrice1 < ProductPrice2)
+				return -1;
+			if (ProductPrice1 > ProductPrice2)
+				return 1;
+			return 0;
 
-        public int compare(Product prod1, Product prod2) {
-            double ProductPrice1 = prod1.calcAvRating();
-            double ProductPrice2 = prod2.calcAvRating();
+		}
 
-            if (ProductPrice1 < ProductPrice2)
-                return 1;
-            if (ProductPrice1 > ProductPrice2)
-                return -1;
-            return 0;
 
-        }
-    };
+	};
+
+	public static Comparator<Product> ProdRatingComparator = new Comparator<Product>() {
+
+		public int compare(Product prod1, Product prod2) {
+			double ProductPrice1 = prod1.calcAvRating();
+			double ProductPrice2 = prod2.calcAvRating();
+
+			if (ProductPrice1 < ProductPrice2)
+				return 1;
+			if (ProductPrice1 > ProductPrice2)
+				return -1;
+			return 0;
+
+		}
+	};
 
 
 }
