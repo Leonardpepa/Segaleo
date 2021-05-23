7d0c8198cac1ff6eb04ac7cb80f01aad1a57f97c
Aravella Lousta <aravellalousta01@gmail.com>
2021-05-23 16:51:37 +0300

    format

--- src/contact/Contact.java
+++ src/contact/Contact.java
@@ -3,32 +3,32 @@ package contact;
 import java.util.ArrayList;
 
 public class Contact {
-	
+
 	private SocialMedia social;
 	private ArrayList<FAQ> faqs;
 	private FAQReader reader;
-	//private Message message;
-	
+	// private Message message;
+
 	public Contact() {
 		this.reader = new FAQReader();
 		this.social = new SocialMedia();
 		this.faqs = reader.getFaqsList();
 	}
-	
+
 	public SocialMedia getSocial() {
 		return social;
 	}
-	
+
 	public void setSocial(SocialMedia social) {
 		this.social = social;
 	}
+
 	public ArrayList<FAQ> getFaqs() {
 		return faqs;
 	}
+
 	public void setFaqs(ArrayList<FAQ> faqs) {
 		this.faqs = faqs;
 	}
-	
-	
 
 }
