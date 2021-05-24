4c3a1dae4a0984d9cf02366653b1a873203a8f33
Leonard Pepa <leopepp4@gmail.com>
2021-05-24 04:22:03 +0300

    made the cart to show tota cost of the order

--- src/gui/windows/CartWindow.java
+++ src/gui/windows/CartWindow.java
@@ -255,7 +255,7 @@ public class CartWindow extends JFrame implements ActionListener{
 		priceHolder.setBackground(Color.WHITE);
 
 		totalLabel = LabelFactory.createLabel(TextResources.total, Color.BLACK, FontFactory.poppins(14));
-		priceLabel = LabelFactory.createLabel("17.00", Color.BLACK, FontFactory.poppins(14));
+		priceLabel = LabelFactory.createLabel(order.calcCost() + "€", Color.BLACK, FontFactory.poppins(14));
 		totalLabel.setBounds(23, 18, 100, 20);
 		priceLabel.setBounds(264, 18, 50, 20);
 
