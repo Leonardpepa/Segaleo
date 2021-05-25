14b350d6e4b7581bbcc87e2bb426c0f5298baaf4
Evita <dasyevita@gmail.com>
2021-05-25 18:48:40 +0300

    add Calendar class

--- src/application/main/Main.java
+++ src/application/main/Main.java
@@ -1,11 +1,12 @@
 
 package application.main;
 
+import calendar.CalendarProgram;
 import gui.windows.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new LoginWindow();
+		new CalendarProgram();
 	}
 }
